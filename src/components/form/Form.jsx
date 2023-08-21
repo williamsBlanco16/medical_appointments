449df30a418f341573888926d0c1456f8bd79f78
williams Blanco <williamsblanco216@gmail.com>
2023-08-21 13:45:06 -0400

    improvement(Patient component): add layout about patient component

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -2,7 +2,7 @@ export const Form = () => {
   return (
     <div className="md:w-1/2 lg:w-2/5">
       <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
-      <p className="text-lg mt-5 text-center mb-10">Agrega pacientes {''} <spam className="text-indigo-600 font-bold">Adminimistrados</spam></p>
+      <p className="text-lg mt-5 text-center mb-10">Agrega pacientes {''} <span className="text-indigo-600 font-bold">Adminimistrados</span></p>
 
       <form className="bg-white shadow-md rounded py-10 px-5 ">
         <div className="mb-5">
--- src/components/patient-list/PatientList.jsx
+++ src/components/patient-list/PatientList.jsx
@@ -1,6 +1,11 @@
+import { Patient } from "../patient"
 
 export const PatientList = () => {
   return (
-    <div className="w-1/2">Patient List</div>
+    <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
+      <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
+      <p className="text-xl mt-5 mb-10 text-center"> Administra tus {''}<span className="text-indigo-600 font-bold">Pacientes y Citas</span></p>
+      <Patient />
+    </div>
   )
 }
